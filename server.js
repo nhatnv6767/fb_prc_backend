be8022b9e62755586f131d7b0b0939335b4456f3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-09-02 21:44:27 +0900

    npm i cors

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("welcome from home");
+    res.send("welcome from home");
 });
 app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
+    res.send("nothing here");
 });
 app.listen(8000, () => {
-  console.log("server is lestining...");
+    console.log("server is listening...");
 });
